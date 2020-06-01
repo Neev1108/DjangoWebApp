f5f8a47670ab8736e8dff54dab14ef8ea61f1c5d
Neev <neevalkumar@gmail.com>
2020-05-31 20:46:51 -0700

    some stuff

--- WebApp/WebApp/View.py
+++ WebApp/WebApp/View.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def go_homePage(request):
-    return render(request, 'TestingSite.html')
+    return render(request, 'accounts/login.html')
